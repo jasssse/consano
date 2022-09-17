145b15bc0715604b7959887e1772deea300cc968
gillian <gillianxu09@gmail.com>
2022-09-17 05:05:35 -0400

    gill first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Jesse
+          Learn Gillian
         </a>
       </header>
     </div>
