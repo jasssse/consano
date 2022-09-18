4e0364c7443f5272a3bbe3ad3afeac3ced33fa1b
gillian <gillianxu09@gmail.com>
2022-09-18 05:30:41 -0400

    fix something

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import './App.css';
 import VoiceRecognition from './voiceRecognition/voiceRecognition';
-import NewEntry from './components/NewEntry';
-import { ChakraProvider } from '@chakra-ui/react'
-import Header from './components/Header';
 
 function App() {
-
 
 return (
   <VoiceRecognition></VoiceRecognition>
--- src/voiceRecognition/voiceRecognition.js
+++ src/voiceRecognition/voiceRecognition.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./voiceRecognition.css";
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
 
 const VoiceRecognition = (props) => {
